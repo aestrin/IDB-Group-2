96779ae1738a3377c3b074cc7d8f21264da5bb24
arjunmbajaj <arjunbajaj@Arjuns-MacBook-Pro-3.local>
2017-03-29 18:43:59 -0500

    made it pretty, fixed starships bc of gaps, padding commit stats

--- getJson.py
+++ getJson.py
@@ -3,7 +3,7 @@ import json
 
 
 NUM_PEOPLE = 88
-NUM_STARSHIPS = 37
+NUM_STARSHIPS = 80
 NUM_PLANETS = 61
 NUM_FILMS = 7
 
@@ -14,21 +14,22 @@ def getJsonDict(model,numModel):
 	for i in range(1,numModel):
 		url = "http://swapi.co/api/{0}/{1}".format(model,i) 	
 		r = requests.get(url)
-		jsondict[i]=r.json()
+		if r.status_code != 404:
+			jsondict[i]=r.json()
 	return jsondict
 
 
-peopleDict = getJsonDict("people",NUM_PEOPLE)
+# peopleDict = getJsonDict("people",NUM_PEOPLE)
 starshipDict = getJsonDict("starships",NUM_STARSHIPS)
-filmDict = getJsonDict("films",NUM_FILMS)
-planetDict = getJsonDict("planets",NUM_PLANETS)
+# filmDict = getJsonDict("films",NUM_FILMS)
+# planetDict = getJsonDict("planets",NUM_PLANETS)
 
 
 with open('allPeople.json', 'w') as outfile:  
-    json.dump(peopleDict, outfile)
+    json.dump(peopleDict, outfile,indent=4)
 with open('allStarships.json', 'w') as outfile:  
-    json.dump(starshipDict, outfile)
+    json.dump(starshipDict, outfile,indent=4)
 with open('allFilms.json', 'w') as outfile:  
-    json.dump(filmDict, outfile)
+    json.dump(filmDict, outfile,indent=4)
 with open('allPlanets.json', 'w') as outfile:  
-    json.dump(planetDict, outfile)
+    json.dump(planetDict, outfile,indent=4)
