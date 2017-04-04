187722644ee13cbd6bc6b0c941bd2f484f957dc7
aestrin <aestrin@utexas.edu>
2017-04-04 18:21:11 -0500

    nav bar and home page front end update

---
+++ app/static/js/nav-bar.js
@@ -0,0 +1,12 @@
+// $(".nav a").on("click", function(){
+//   $(".nav").find(".active").removeClass("active");
+//   $(this).parent().addClass("active");
+// });
+
+// $('.nav li a').click(function(e) {
+//   var $this = $(this);
+//   if (!$this.hasClass('active')) {
+//     $this.addClass('active');
+//   }
+//   e.preventDefault();
+// });
