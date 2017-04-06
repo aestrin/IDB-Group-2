f365d900d3e5ceeb7423eb421f904659ebaddbb9
Sam Weismuller <sweis@cs.utexas.edu>
2017-04-05 19:13:50 -0500

    Seperated modular code for sorting.

--- app/templates/table_builder.jsx
+++ app/templates/table_builder.jsx
@@ -1,3 +1,92 @@
+var size = 6;
+
+function load(error, response)
+{
+    if(error == null)
+    {
+        console.log(response);
+        document.getElementById("grid_location").innerHTML = "";
+        size = response.length;
+        renderTable(buildModels(response, true), document.getElementById("grid_location"), 3);
+    }
+}
+
+// common code
+var filter = null;
+var filterBy = null;
+var sort = null;
+var up = 1;
+var page = 1;
+
+function buildURL()
+{
+    var url = baseUrl;
+    url += "?page="+page;
+    if(filter != null)
+        url += "&filterBy="+filterBy+"&filter="+filter;
+    if(sort != null)
+    {
+        if(up)
+            url +="&sortUp="+sort;
+        else
+            url +="&sortDown="+sort;
+    }
+    url += "&raw";
+    return url;
+}
+
+function reload()
+{
+    var url = buildURL();
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.responseType = 'json';
+    req.onload = function() {
+      var status = req.status;
+      if (status == 200) {
+        load(null, req.response);
+      } else {
+        load(status);
+      }
+    };
+    req.send();
+}
+
+function sortBy(value)
+{
+    sort = value;
+    page = 1;
+    reload();
+}
+
+function sortDir(value)
+{
+    up = value;
+    page = 1;
+    reload();
+}
+
+function nextPage()
+{
+    page += 1;
+    reload();
+}
+
+function prevPage()
+{
+    if(page != 1)
+        page -= 1;
+    reload();
+}
+
+function filterPage(filtBy, filt)
+{
+    filterBy = filtBy;
+    filter = filt;
+    page = 1;
+    reload();
+}
+
 function GenericList(props) {
     var list = props.list;
     var listItems = list.map((ele) => <li>{ele}</li>);
