807a1f3994c1c8c1ba64342a30dfdec6395028da
Pushkar Lanka <pushkar.lanka@gmail.com>
2017-04-15 20:22:22 -0500

    added CORS stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 from config import Config
 
 application = Flask(__name__, static_url_path='/app/static')
+CORS(application)
 
 config = Config()
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
