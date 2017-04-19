91657245878d4c2c6c403ba4b2ef36ffa368a24b
aestrin <aestrin@utexas.edu>
2017-04-19 18:43:13 -0500

    still working on visual, finished making search look better

---
+++ app/scraper/visualscraper.py
@@ -0,0 +1,20 @@
+import requests
+import re
+import json
+
+response = requests.get("http://reddifulapi.me/api/users?page=1").json()[0]
+response+=(requests.get("http://reddifulapi.me/api/users?page=2").json())[0]
+response+=(requests.get("http://reddifulapi.me/api/users?page=3").json())[0]
+response+=(requests.get("http://reddifulapi.me/api/users?page=4").json())[0]
+
+
+t = []
+for item in response:
+	t.append((item.get("name"),item.get("link_karma") ,item.get("comment_karma")))
+
+jsondict =  dict([ (k, [v, w]) for k, v, w in t ])
+
+with open('visual.json', 'w') as outfile:  
+    json.dump(jsondict, outfile,indent=4)
+
+
